28fe916b91ecfca175d404d7b755d6f444b15501
StinkyKronos <hemuhemu85672@gmail.com>
2024-08-22 15:18:43 +0530

    Started homepage.

--- src/app/component/NavBar.jsx
+++ src/app/component/NavBar.jsx
@@ -1,3 +1,17 @@
+'use client'
+
+import { useRouter } from "next/navigation";
+
 export default function NavBar() {
-  return <div>NavBar</div>;
+	const router = useRouter();
+
+	return (
+		<div className="w-screen flex justify-between px-10 py-5 text-black">
+			<h2 className="poppins-medium text-2xl">ApnaKamra</h2>
+			<div className="flex gap-5">
+				<p className="poppins-medium" >Login</p>
+				<p className="poppins-medium">About Us</p>  
+			</div>
+		</div>
+	);
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,34 +4,43 @@ import { useRouter } from "next/navigation";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+
+import NavBar from "./component/NavBar";
+
 export default function Home() {
-  const [user, setUser] = useState(null);
-  const router = useRouter();
-  const [loading, setLoading] = useState(true);
-  const supabase = createClientComponentClient();
+	const [user, setUser] = useState(null);
+	const router = useRouter();
+	const [loading, setLoading] = useState(true);
+	const supabase = createClientComponentClient();
 
-  useEffect(() => {
-    const getUser = async () => {
-      const {
-        data: { user },
-      } = await supabase.auth.getUser();
-      setUser(user);
-      setLoading(false);
-    };
-    getUser();
-  });
+	useEffect(() => {
+		const getUser = async () => {
+			const {
+				data: { user },
+			} = await supabase.auth.getUser();
+			setUser(user);
+			setLoading(false);
+		};
+		getUser();
+	});
 
-  if (user) return <main>rfe</main>;
+	if (user) return <main>rfe</main>;
 
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <NavBar/>
-      <div
-        className="bg-white/10 px-5 py-2 rounded-md"
-        onClick={() => router.push("/login")}
-      >
-        Login
-      </div>
-    </main>
-  );
+	return (
+		<main className="h-screen w-screen bg-white">
+			<NavBar />
+
+			<div className="mt-5 flex h-screen w-screen justify-center">
+				<div className="w-11/12 h-3/4 bg-cover bg-hero-banner rounded-xl flex flex-col items-start justify-center px-10 gap-3">
+					<p className="text-3xl poppins-medium">
+						find your <span className="text-[#FC813E]">home</span> like room now
+					</p>
+					<div className="w-full h-14 bg-white rounded-2xl flex justify-between items-center px-5">
+            <input type="text" placeholder="Search" className="focus:outline-none w-full text-black poppins-medium h-3/4" />
+            <img src="images/search.svg" alt="" width={32} height={32} />
+          </div>
+				</div>
+			</div>
+		</main>
+	);
 }
