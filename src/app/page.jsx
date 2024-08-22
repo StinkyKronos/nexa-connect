c9ef180e27ffde65fbed474a670b44550bd4a8de
SOME1HING <yashprakash2005@gmail.com>
2024-08-23 02:19:45 +0530

    I need to sleep.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,69 +1,93 @@
 "use client";
 
-import { useRouter } from "next/navigation";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
 
 import NavBar from "./component/NavBar";
+import Search from "./ui/search";
 
 export default function Home() {
-	const [user, setUser] = useState(null);
-	const router = useRouter();
-	const [loading, setLoading] = useState(true);
-	const supabase = createClientComponentClient();
+  const [user, setUser] = useState(null);
+  const router = useRouter();
+  const [loading, setLoading] = useState(true);
+  const supabase = createClientComponentClient();
 
-	useEffect(() => {
-		const getUser = async () => {
-			const {
-				data: { user },
-			} = await supabase.auth.getUser();
-			setUser(user);
-			setLoading(false);
-		};
-		getUser();
-	});
+  const searchParams = useSearchParams();
+  let search = searchParams.get("search") ? searchParams.get("search") : "";
 
-	if (user) return <main>rfe</main>;
+  useEffect(() => {
+    const getUser = async () => {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+      setUser(user);
+      setLoading(false);
+    };
+    getUser();
+  });
 
-	return (
-		<main className="h-fit w-screen bg-inherit">
-			<NavBar />
-			<div className="flex h-screen w-screen justify-center items-center">
-				<div className="w-4/5 h-3/4 bg-cover bg-hero-banner rounded-xl flex flex-col items-start justify-center px-10 gap-3">
-					<p className="text-3xl poppins-medium">
-						find your <span className="text-[#FC813E]">home</span> like room now
-					</p>
-					<div className="w-full h-14 bg-white rounded-2xl flex justify-between items-center px-5">
-						<input type="text" placeholder="Search" className="focus:outline-none w-full text-black poppins-medium h-3/4" />
-						<img src="images/search.svg" alt="" width={32} height={32} />
-					</div>
-				</div>
-			</div>
+  if (user) return <main>rfe</main>;
 
-			<div className="w-screen h-40 bg-[#FCEBE2] flex items-center justify-around px-10">
-				<p className="text-black text-3xl poppins-semibold">12k+ clients</p>
-				<p className="text-black text-3xl poppins-semibold">25+ cities</p>
-				<p className="text-black text-3xl poppins-semibold">875+ hostels</p>
-				<p className="text-black text-3xl poppins-semibold">4k+ visits</p>
-			</div>
+  if (search == "")
+    return (
+      <main className="h-fit w-screen bg-inherit">
+        <NavBar />
+        <div className="flex h-screen w-screen justify-center items-center">
+          <div className="w-4/5 h-3/4 bg-cover bg-hero-banner rounded-xl flex flex-col items-start justify-center px-10 gap-3">
+            <p className="text-3xl poppins-medium">
+              find your <span className="text-[#FC813E]">home</span> like room
+              now
+            </p>
+            <Search />
+          </div>
+        </div>
 
-			<div className="grid grid-cols-2 h-screen w-screen justify-center items-center px-32 py-16 gap-y-14">
-				<div className="w-full h-full">
-					<h2 className="poppins-semibold text-[#FC813E] text-7xl">HOME</h2>
-					<p className="poppins-semibold text-black text-5xl mt-3">far from home</p>
-					<p className="poppins-medium text-black text-2xl mt-10">Bring a box full of hopes, dreams, ambitions and of course, your personal belongings. Everything else has already been taken care of.</p>
-				</div>
-				<div className="w-full h-full bg-[url('/images/tile1.png')]">
-					{/* <img className="h-full" src="images/tile1.jpg" alt="" /> */}
-				</div>
-				<div className="w-full h-full ">
-					<img className="h-full" src="images/tile2.jpg" alt="" />
-				</div>
-				<div className="w-full h-full ">
-					<img className="h-full" src="images/tile3.jpg" alt="" />
-				</div>
-			</div>
-		</main>
-	);
+        <div className="w-screen h-40 bg-[#FCEBE2] flex items-center justify-around px-10">
+          <p className="text-black text-3xl poppins-semibold">12k+ clients</p>
+          <p className="text-black text-3xl poppins-semibold">25+ cities</p>
+          <p className="text-black text-3xl poppins-semibold">875+ hostels</p>
+          <p className="text-black text-3xl poppins-semibold">4k+ visits</p>
+        </div>
+
+        <div className="grid grid-cols-2 h-screen w-screen justify-center items-center px-32 py-16 gap-y-14">
+          <div className="w-full h-full">
+            <h2 className="poppins-semibold text-[#FC813E] text-7xl">HOME</h2>
+            <p className="poppins-semibold text-black text-5xl mt-3">
+              far from home
+            </p>
+            <p className="poppins-medium text-black text-2xl mt-10">
+              Bring a box full of hopes, dreams, ambitions and of course, your
+              personal belongings. Everything else has already been taken care
+              of.
+            </p>
+          </div>
+          <div className="w-full h-full bg-[url('/images/tile1.png')]">
+            {/* <img className="h-full" src="images/tile1.jpg" alt="" /> */}
+          </div>
+          <div className="w-full h-full ">
+            <img className="h-full" src="images/tile2.jpg" alt="" />
+          </div>
+          <div className="w-full h-full ">
+            <img className="h-full" src="images/tile3.jpg" alt="" />
+          </div>
+        </div>
+      </main>
+    );
+
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`text-2xl`}>Invoices</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search invoices..." />
+      </div>
+      <Suspense key={search}></Suspense>
+      <div className="mt-5 flex w-full justify-center">
+        {/* <Pagination totalPages={totalPages} /> */}
+      </div>
+    </div>
+  );
 }
---
+++ src/app/ui/search.jsx
@@ -0,0 +1,38 @@
+"use client";
+
+import Image from "next/image";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { useState } from "react";
+
+export default function Search() {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  const [query, setQuery] = useState();
+
+  function handleSearch() {
+    const params = new URLSearchParams(searchParams);
+    if (query) {
+      params.set("search", query);
+    } else {
+      params.delete("search");
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }
+
+  return (
+    <div className="w-full h-14 bg-white rounded-2xl flex justify-between items-center px-5">
+      <input
+        type="text"
+        placeholder="Search"
+        className="focus:outline-none w-full text-black poppins-medium h-3/4"
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key == "Enter") handleSearch();
+        }}
+        defaultValue={searchParams.get("search")?.toString()}
+      />
+      <Image src="images/search.svg" alt="" width={32} height={32} />
+    </div>
+  );
+}
